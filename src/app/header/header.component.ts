f69677380eb99d9f667f6a5d44b4e82849fbee00
a625521669 <625521669@qq.com>
2017-03-07 18:22:31 +0800

    用户模块

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,25 +1,57 @@
 import { AuthService } from '../auth.service';
 import { Component } from '@angular/core';
+import { UserService } from '../user/user.service';
+
 import {
     Router,
     ActivatedRouteSnapshot,
     RouterStateSnapshot
 } from '@angular/router';
 
+
+import 'rxjs/add/operator/switchMap';
+
+declare const $: any;
+
 @Component({
     selector: 'Header',
     templateUrl: './header.component.html',
+    providers: [UserService]
 })
 
 export class HeaderComponent {
-    constructor(public authService: AuthService, private router: Router) {
+    constructor(public authService: AuthService, private router: Router, private userService: UserService) {
     }
 
+    
     userName = this.authService.userName != undefined ? this.authService.userName : "请登陆";
 
-    logout(){
+    logout() {
         this.authService.logout();
         // this.authService.redirectUrl = this.router
         this.router.navigate(["/login"]);
+    }
+
+
+    //显示密码修改面板
+    showPasswprdChangeFn = () => {
+        $("#myPswModal").modal("show");
+    }
+
+    //修改密码
+    changePasswordFn = () => {
+        let data = new FormData(document.forms['myPswData']);
+
+        data.append('userId', localStorage.getItem("userId"));
+        
+        this.userService.changePassword(data)
+            .then(res => {
+                if (res.success) {
+                    alert("密码修改成功！");
+                    $("#myPswModal").modal("hide");
+                } else {
+                    alert(res.reason);
+                }
+            })
     }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,43 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { UserService } from '../user/user.service';
+
+import 'rxjs/add/operator/switchMap';
+
 
 @Component({
     selector: 'Home',
     templateUrl: './home.component.html',
+    providers: [UserService]
 })
 
-export class HomeComponent { }
+export class HomeComponent {
+    public data = {};
+
+    //构造方法
+    constructor(private userService: UserService) {
+    }
+
+    ngOnInit() {
+        this.userService.getInfo(localStorage.getItem('userId'))
+            .then(res => {
+                this.data = res;
+            })
+    }
+
+    saveData() {
+        let data = new FormData(document.forms['formData']);
+
+        data.append('UserID', localStorage.getItem('userId'));
+
+        this.userService.modifyUser(data)
+            .then((result) => {
+                if (result.success) {
+                    alert('修改成功！');
+                } else {
+                    alert(result.Reason);
+                }
+            })
+    }
+
+};
